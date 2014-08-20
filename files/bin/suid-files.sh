c70a6b69a614c4d8bc2958602915847f7546ac81
Surfraz Ahmed <surfraz@gmail.com>
2014-08-20 09:53:13 +0100

    [SA] - added suid files report and updated application filters

---
+++ files/bin/suid-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+umask 0077
+
+if [ -r /var/log/zabbix/suid-sgid-files.txt ]; then
+  mv -f /var/log/zabbix/suid-sgid-files.txt /var/log/zabbix/suid-sgid-files.txt.old
+fi
+
+for PART in $(grep -v '^#' /etc/fstab | awk '($6 != "0") { print $2 }' ); do
+  find $PART -xdev \( -perm -04000 -o -perm -02000 \) -type f -exec md5sum {} \;
+done > /var/log/zabbix/suid-sgid-files.txt

79b58eb5eb9706836bbdba8a2c767872b50209ce
Surfraz Ahmed <surfraz@gmail.com>
2014-09-09 08:41:08 +0100

    [SA] - fix file permissions on suid-files report generated files

--- files/bin/suid-files.sh
+++ files/bin/suid-files.sh
@@ -4,11 +4,11 @@ umask 0077
 
 if [ -r /var/log/zabbix/suid-sgid-files.txt ]; then
   mv -f /var/log/zabbix/suid-sgid-files.txt /var/log/zabbix/suid-sgid-files.txt.old
-  chown zabbix /var/log/zabbix/suid-sgid-files.txt.old
 fi
 
 for PART in $(grep -v '^#' /etc/fstab | awk '($6 != "0") { print $2 }' ); do
   find $PART -xdev \( -perm -04000 -o -perm -02000 \) -type f -exec md5sum {} \;
 done > /var/log/zabbix/suid-sgid-files.txt
 
-chown zabbix /var/log/zabbix/suid-sgid-files.txt
+# make it readable by zabbix
+chown zabbix /var/log/zabbix/suid-sgid-files.*
